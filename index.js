42f98fc3ddd593d7ec2f703cba278b4cc8c38513
STSumu <sabihatahsinsumu@gmail.com>
2025-11-22 20:39:38 +0600

    added application post route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const cors=require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express();
 const port=process.env.PORT || 5000;
 
@@ -29,11 +29,28 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     const jobCollection=client.db('Job-Portal').collection('jobs');
+    const jobApplicationCollection=client.db('Job-Portal').collection('jobApplications');
 
     app.get('/jobs',async(req,res)=>{
         const jobs=jobCollection.find();
         const result=await jobs.toArray();
         res.send(result);
+    })
+    app.get('/jobs/:id',async(req,res)=>{
+      const query=new ObjectId(req.params.id);
+      const result=await jobCollection.findOne(query);
+      res.send(result);
+    });
+    app.get('/application',async(req,res)=>{
+      const email=req.query.email;
+      const query={applicantEmail:email};
+      const result=await jobApplicationCollection.find(query).toArray();
+      res.send(result);
+    })
+    app.post('/jobApply',async(req,res)=>{
+      const application=req.body;
+      const result=await jobApplicationCollection.insertOne(application);
+      res.send(result);
     })
   } finally {
     // Ensures that the client will close when you finish/error
